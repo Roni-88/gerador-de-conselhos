c439277647cc49bf804c116efa1ce2c609bb7baf
Roni 88 <ronix88x@hotmail.com>
2022-09-30 23:11:47 -0300

    consumindo api advices

---
+++ src/app.js
@@ -0,0 +1,13 @@
+document.getElementById ('btn').addEventListener ('click', geradorDeConselhos)
+
+const conselho = document.getElementById ('conselho')
+const conteudo = document.getElementById ('conteudo')
+
+async function geradorDeConselhos () {
+    const url = 'https://api.adviceslip.com/advice'
+    const conselhoResposta = await fetch (url)
+    const conselhoGerado = await conselhoResposta.json()
+    conselho.innerText = `CONSELHO # ${conselhoGerado.slip.id}`
+    conteudo.innerText = `${conselhoGerado.slip.advice}`
+}
+geradorDeConselhos ()
